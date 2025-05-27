ea0510a30975b329076450d417d74f70a278cb9a
ben <ben.d.godfrey2002@gmail.com>
2025-05-27 09:09:33 +0100

    create spring boot project files

---
+++ web-interface/src/test/java/com/framepi/web_interface/WebInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framepi.web_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
